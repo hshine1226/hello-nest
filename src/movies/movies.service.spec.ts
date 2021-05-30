0160d113e482e748d413476bb925a955fa4a0094
hshine1226 <hshine1226@gmail.com>
2021-05-30 21:20:12 +0900

    Add sample unit test code

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
+// 테스트를 설명
 describe('MoviesService', () => {
   let service: MoviesService;
 
+  // 테스트 실행 전에 수행
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
@@ -12,7 +14,12 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+  // 테스트 실행 부분
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be 4', () => {
+    expect(2 + 2).toEqual(4);
   });
 });
