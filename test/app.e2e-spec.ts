685f160a921b9d3504f8251d9af4e44e82e7bcee
hshine1226 <hshine1226@gmail.com>
2021-06-12 22:51:39 +0900

    Add movies e2e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,24 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
+  });
+
+  // MDM Web Docs( https://developer.mozilla.org/ko/docs/Web/HTTP/Status/404)
+  describe('/movies', () => {
+    // OK http response code is 200
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+    // Created http response code is 201
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({ title: 'test', year: '2021', genres: ['test'] })
+        .expect(201);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
   });
 });
